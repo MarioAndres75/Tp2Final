546bbf8d448e25e66d099a0ba290ccbcb4a5f7c9
MarioAndres75 <102887327+MarioAndres75@users.noreply.github.com>
2022-10-31 20:10:58 -0300

    version 1.0

--- app/src/main/java/com/example/practicaappy/MainActivity.kt
+++ app/src/main/java/com/example/practicaappy/MainActivity.kt
@@ -27,49 +27,36 @@ lateinit var boton:Button
     fun crearPalabra(): String {
         val palabra= "CELULAR"
         var palabraFinal= arrayOfNulls<Char>(palabra.length)
-        var palabraFinal2= arrayOfNulls<Char>(palabra.length)
-
         var posicionRepetidaNo= arrayOfNulls<Int>(palabra.length)
         val cantidadDeLetras =palabra.length
         var contador=0
         var posicion =0
         var letra : Char
         var retorno=""
-        var noSeRepite=true
-        var repetido=0;
+        var salir=0
         while (contador<cantidadDeLetras ) {
-
-           posicion = (Math.random() * cantidadDeLetras).toInt()
-     // posicion=contador
-            posicionRepetidaNo[contador] = posicion
-
-            while (repetido!=1) {
-                posicionRepetidaNo.forEach {
-                    posicionRepetidaNo[contador]= posicion
-                    repetido=1
-                    var actual = "$it"
-                    if (actual == posicion.toString()) {
-                        noSeRepite = false
-                        posicion++
-                        repetido=0
-                        if(posicion==palabra.length)posicion=0
-                    }
-
-                }
-
+     while (salir==0) {
+                posicion = (Math.random() * cantidadDeLetras).toInt()
+               if (posicionRepetidaNo.contains(posicion))
+               else {
+                    posicionRepetidaNo[contador] = posicion
+                    salir=1
+               }
             }
-                if(noSeRepite) {
+            salir=0
                 letra = palabra.get(posicion) //busca una letra de la palabra original
                 palabraFinal[contador] = letra  // coloca la letra en la palabra desordenada
                 contador++
-            }
-            noSeRepite=true
+
+
 
 
         }
         palabraFinal.forEach {
+
          retorno="$retorno$it"  //arma la palabra en un string
         }
+
         return retorno
     }
 
